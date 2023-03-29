f2c5b57e0f5b6be245df9627dec34937a9f16974
Jhorman Orozco <jsojhor@gmail.com>
2023-03-28 20:12:28 -0500

    upload proyect

---
+++ components/layout/Layout.tsx
@@ -0,0 +1,32 @@
+import Head from 'next/head';
+import { FC } from 'react';
+import { Navbar } from '../ui';
+
+type Props = {
+  children: React.ReactNode;
+  title?: string;
+};
+
+export const Layout: FC<Props> = ({ children, title }) => {
+  return (
+    <>
+      <Head>
+        <title>{title || 'Pokemon App'}</title>
+        <meta name="author" content="Jhorman Orozco" />
+        <meta
+          name="description"
+          content={`Información sobre el pokémon ${title}`}
+        />
+        <meta name="keywords" content={`${title}, pokemon, pokedex`} />
+      </Head>
+      <Navbar />
+      <main
+        style={{
+          padding: '0px 20px',
+        }}
+      >
+        {children}
+      </main>
+    </>
+  );
+};
